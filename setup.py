cff2cf1a14f5b0b4d889c13355046c2f881700ab
Szilveszter István <istvan.szilveszter@equal1.com>
2026-02-25 14:45:56 +0200

    another try

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, find_packages
 
-# Get all packages recursively and prefix them with parent package name
-packages = ['qedc_appbms'] + [f'qedc_appbms.{pkg}' for pkg in find_packages()]
-
 setup(
     name="qedc-appbms",
     description="QED-C Application Oriented Benchmarks package.",
     version="0.1.0",
-    packages=packages,
     package_dir={'qedc_appbms': '.'},
+    packages=['qedc_appbms'] + ['qedc_appbms.' + pkg for pkg in find_packages(where='.')],
 )
