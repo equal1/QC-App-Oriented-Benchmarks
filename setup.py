6ac2b7c765c632b3430a3d7601545b0448ba7cf1
Szilveszter István <istvan.szilveszter@equal1.com>
2026-02-25 14:27:04 +0200

    attempt to create one package

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+# Get all packages and prefix them with parent package name
+packages = ['qedc_appbms']
+for pkg in find_packages():
+    packages.append(f'qedc_appbms.{pkg}')
+
 setup(
-    name="qedc-appbms", 
+    name="qedc-appbms",
     description="QED-C Application Oriented Benchmarks package.",
     version="0.1.0",
-    packages=find_packages(),
+    packages=packages,
+    package_dir={'qedc_appbms': '.'},
 )
